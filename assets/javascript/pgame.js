71e2a358986fd769d5cc337af143bf343f1faac5
94natem <nate_m@live.com>
2018-11-10 03:30:31 -0700

    Finished Css Html and Js for word-guess-game

---
+++ assets/javascript/pgame.js
@@ -0,0 +1,119 @@
+
+
+const maxTries = 10;            // Maximum number of tries player has
+
+var guessedLetters = [];        // Stores the letters the user guessed
+var currentWordIndex;           // Index of the current word in the array
+var guessingWord = [];          // This will be the word we actually build to match the current word
+var remainingGuesses = 0;       // How many tries the player has left
+var gameStarted = false;        // Flag to tell if the game has started
+var hasFinished = false;        // Flag for 'press any key to try again'     
+var wins = -1;                   // How many wins has the player racked up
+var wordChoices = ["rogue", "warrior", "mage", "hunter", "shaman", "paladin", "warlock", "monk", "priest", "druid", "ogre", "orc", "elf", "dragon", "tauren", "troll", "dwarf", "gnome", "goblin"]
+
+
+// Reset our game-level variables
+function resetGame() {
+    remainingGuesses = maxTries;
+    gameStarted = false;
+
+    // Use Math.floor to round the random number down to the nearest whole.
+    currentWordIndex = Math.floor(Math.random() * (wordChoices.length));
+
+    // Clear out arrays
+    guessedLetters = [];
+    guessingWord = [];
+
+    // Build the guessing word and clear it out
+    for (var i = 0; i < wordChoices[currentWordIndex].length; i++) {
+        guessingWord.push(" _ ");
+    }
+    // Hide game over and win images/text
+    document.getElementById("pressKeyTryAgain").style.display = "none";
+    document.getElementById("gameoverText").style.display = "none";
+    document.getElementById("youwinText").style.display = "none";
+
+    // Show display
+    updateDisplay();
+};
+
+//  Updates the display on the HTML Page
+function updateDisplay() {
+
+    document.getElementById("totalWins").innerText = " Total Wins " + wins;
+    document.getElementById("currentWord").innerText = "";
+    for (var i = 0; i < guessingWord.length; i++) {
+        document.getElementById("currentWord").innerText += guessingWord[i];
+    }
+    document.getElementById("remainingGuesses").innerText = " Remaining Guesses: " + remainingGuesses;
+    document.getElementById("guessedLetters").innerText = " You have already guessed: " + guessedLetters;
+    if(remainingGuesses <= 0) {
+        document.getElementById("gameoverText").style.display = "block";
+        document.getElementById("pressKeyTryAgain").style.display = "block";
+        hasFinished = true;
+    }
+};
+ 
+
+document.onkeydown = function(event) {
+    // If we finished a game, dump one keystroke and reset.
+    if(hasFinished) {
+        resetGame();
+        hasFinished = false;
+    } else {
+        // Check to make sure a-z was pressed.
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+            makeGuess(event.key.toLowerCase());
+        }
+    }
+};
+
+function makeGuess(letter) {
+    if (remainingGuesses > 0) {
+        if (!gameStarted) {
+            gameStarted = true;
+        }
+
+        // Make sure we didn't use this letter yet
+        if (guessedLetters.indexOf(letter) === -1) {
+            guessedLetters.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+    
+    updateDisplay();
+    checkWin();
+};
+
+// This function takes a letter and finds all instances of 
+// appearance in the string and replaces them in the guess word.
+function evaluateGuess(letter) {
+    // Array to store positions of letters in string
+    var positions = [];
+
+    // Loop through word finding all instances of guessed letter, store the indicies in an array.
+    for (var i = 0; i < wordChoices[currentWordIndex].length; i++) {
+        if(wordChoices[currentWordIndex][i] === letter) {
+            positions.push(i);
+        }
+    }
+
+    // if there are no indicies, remove a guess and update the hangman image
+    if (positions.length <= 0) {
+        remainingGuesses--;
+    } else {
+        // Loop through all the indicies and replace the '_' with a letter.
+        for(var i = 0; i < positions.length; i++) {
+            guessingWord[positions[i]] = letter;
+        }
+    }
+};
+
+function checkWin() {
+    if(guessingWord.indexOf(" _ ") === -1) {
+        document.getElementById("youwinText").style.display = "block";
+        document.getElementById("pressKeyTryAgain").style.display = "block";
+        wins++;
+        hasFinished = true;
+    }
+};
